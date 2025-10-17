ce8e3ef2463e515dbc38221dc82fde788cc7d821
Joshua Iruobe <joshuairuobe@gmail.com>
2025-10-17 01:04:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <i className="fa-solid fa-bars"></i>
+      <h1>Joshua Iruobe's Portfolio</h1>
+      <p>Web developer passionate about clean design and code</p>
+      <h2>Projects</h2>
+      <button><a href="https://todolist-jiruobe.netlify.app/">To Do List</a></button>
+      <button><a href="https://ji-calculator.netlify.app/">Calculator</a></button>
+      <button><a href="https://weather-jiruobe.netlify.app/">Weather App</a></button>
+    </>
+  )
+}
+
+export default App
